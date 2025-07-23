fd34a13265d2daa06140427612005fd5297bf67d
NishaM2 <nisham2522@gmail.com>
2025-07-23 23:17:22 +0530

    Resolved merge conflict in server.js

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,9 @@ import cors from 'cors'
 import { connectDB } from './config/db.js';
 import foodRouter from './routes/foodRoute.js';
 import 'dotenv/config';
+import favoritesRoute from './routes/favoriteRoute.js';
+import userRoute from './routes/userRoutes.js'
+
 // app config
 const app = express();
 const port = 4000;
@@ -17,6 +20,8 @@ connectDB();
 
 // api endpoints
 app.use("/api/food",foodRouter);
+app.use('/api/favorites', favoritesRoute);
+app.use('/api/users', userRoute);
 
 app.get('/',(req,res)=>{
     res.send("api working");
