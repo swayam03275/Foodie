84143fec7ad23fc13d3e0e26b3c4111a1fdc69d3
ajinkya8010 <ajinkya8975@gmail.com>
2025-07-30 23:34:49 +0530

    fix:Search bar position fixed in home page

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import './Home.css';
 import Header from '../../components/Header/Header';
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu';
 import FoodDisplay from '../../components/FoodDisplay/FoodDisplay';
+import SearchBar from "../../components/SearchBar/SearchBar";
 
 const Home = () => {
   const [category, setCategory] = useState('All');
@@ -19,6 +20,7 @@ const Home = () => {
   
   return (
     <div className="home-page">
+      <SearchBar />
       <Header />
       <ExploreMenu category={category} setCategory={setCategory} />
       <FoodDisplay category={category} />
