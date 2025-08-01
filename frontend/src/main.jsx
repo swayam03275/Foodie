a8bca54d923962910fd122193475746767700a23
Abhishek Farswal <136872441+Abhishek2634@users.noreply.github.com>
2025-08-01 14:47:48 +0530

    Revert "Fixed time delay in google login"

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,25 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
 import './index.css'
 import {BrowserRouter} from 'react-router-dom'
 import StoreContextProvider from './components/context/StoreContext.jsx'
 
-// Import both versions
-import App from './App.jsx' // Original App
-import FirebaseApp from './FirebaseApp.jsx' // Firebase-enhanced App
-
-// Configuration flag - set to true to use Firebase auth, false for original
-const USE_FIREBASE_AUTH = true;
-
-// Choose which app to render
-const AppComponent = USE_FIREBASE_AUTH ? FirebaseApp : App;
-
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
-      <StoreContextProvider>
-        <AppComponent />
-      </StoreContextProvider>
-    </BrowserRouter>
-  </StrictMode>
+  <BrowserRouter>
+    <StoreContextProvider>
+      <App />
+    </StoreContextProvider>
+  </BrowserRouter>
 )
